b4cfae5b954224d9e2a2ea3789a46f430889c596
unknown <izazahmed0177@gmail.com>
2022-10-26 11:36:16 +0600

    register page degine complite

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [error,setError]=useState('');
+
+    const handeleSubmit=event=>{
+        event.preventDefault();
+        const form=event.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log(email,password);
+    }
+
+
+
+
+
     return (
         <div className='bg-slate-800 flex justify-center p-9'>
 
-       
-
-
-
-
         <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-900 text-gray-100">
 	<h1 className="text-2xl font-bold text-center">Login</h1>
-	<form novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+	<form onSubmit={handeleSubmit} novalidate="" action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
 
 		<div className="space-y-1 text-sm">
 			<label for="userEmail" className="block text-gray-400">User Email</label>
-			<input type="Email" name="email" id="email" placeholder="User Email" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
+			<input type="Email" name="email"  placeholder="User Email" required className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
 		</div>
 
 		<div className="space-y-1 text-sm">
 			<label for="password" className="block text-gray-400">Password</label>
-			<input type="password" name="password" id="password" placeholder="Password" className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
+			<input type="password" name="password"  placeholder="Password" required className="w-full px-4 py-3 rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400" />
             
 			<div className="flex justify-end text-xs text-gray-400">
 				{/* <a rel="noopener noreferrer" href="#">Forgot Password?</a> */}
 			</div>
 		</div>
-		<button className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400 hover:bg-violet-600 ">Sign in</button>
+		<button type='submit' className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400 hover:bg-violet-600 ">Sign in</button>
 	</form>
 	<div className="flex items-center pt-4 space-x-1">
 		<div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
