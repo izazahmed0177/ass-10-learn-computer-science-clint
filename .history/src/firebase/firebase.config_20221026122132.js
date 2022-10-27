4935c250456eafe641eddbbe19d3f8e5bc6f67f2
unknown <izazahmed0177@gmail.com>
2022-10-27 19:21:03 +0600

    possage env add complite

--- .history/src/firebase/firebase.config_20221026122132.js
+++ .history/src/firebase/firebase.config_20221026122132.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyANc6RBjX2PjkRNQNXYeG6RCoHYLKPVWJU",
+    authDomain: "learn-computer-science-70d7b.firebaseapp.com",
+    projectId: "learn-computer-science-70d7b",
+    storageBucket: "learn-computer-science-70d7b.appspot.com",
+    messagingSenderId: "204336588984",
+    appId: "1:204336588984:web:ad0d9235c8e9814ea3ec4d"
 };
 
 // Initialize Firebase
